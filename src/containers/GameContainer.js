956212620277826b459b49aa6e0da2dd3497d8bf
KJeffree <k.jeffree@gmail.com>
2019-10-30 16:00:09 +0000

    refactored

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -21,10 +21,9 @@ class GameContainer extends Component {
 
   handleSquareSelected(squaresplayed){
     if (this.state.winner == null){
-    console.log("squares", this.state.squares);
-    const newSquare = this.state.squares.map((square, index) => {
+      const newSquare = this.state.squares.map((square, index) => {
       if(index == squaresplayed && square == 0){
-        if (this.state.currentMarker == true){
+        if (this.state.currentMarker === true){
           this.setState({currentMarker: false})
           return square = 1;
         }
@@ -32,34 +31,30 @@ class GameContainer extends Component {
         return square = 2;
       }
       return square;
-    })
-      console.log("newSquare", newSquare);
-      this.checkWinner(newSquare)
+      })
+    this.checkWinner(newSquare)
     this.setState({squares: newSquare});
-  } else {
+    } else {
     return null;
-  }
-
+    }
   }
 
   checkWinner(allSquares){
-    // const allSquares = this.state.squares
-    console.log('state when clicked', allSquares.slice())
-    if (allSquares[0] == allSquares[1] && allSquares[1] == allSquares[2] && allSquares[0] != 0){
+    if (allSquares[0] === allSquares[1] && allSquares[1] === allSquares[2] && allSquares[0] !== 0){
       this.setState({winner: allSquares[0]});
-    }else if(allSquares[3] == allSquares[4] && allSquares[4] == allSquares[5] && allSquares[3] != 0){
+    }else if(allSquares[3] === allSquares[4] && allSquares[4] === allSquares[5] && allSquares[3] !== 0){
       this.setState({winner: allSquares[3]});
-    }else if(allSquares[6] == allSquares[7] && allSquares[7] == allSquares[8] && allSquares[6] != 0){
+    }else if(allSquares[6] === allSquares[7] && allSquares[7] === allSquares[8] && allSquares[6] !== 0){
       this.setState({winner: allSquares[6]});
-    }else if(allSquares[0] == allSquares[3] && allSquares[3] == allSquares[6] && allSquares[0] != 0){
+    }else if(allSquares[0] === allSquares[3] && allSquares[3] === allSquares[6] && allSquares[0] !== 0){
       this.setState({winner: allSquares[0]});
-    }else if(allSquares[1] == allSquares[4] && allSquares[4] == allSquares[7] && allSquares[1] != 0){
+    }else if(allSquares[1] === allSquares[4] && allSquares[4] === allSquares[7] && allSquares[1] !== 0){
       this.setState({winner: allSquares[1]});
-    }else if(allSquares[2] == allSquares[5] && allSquares[5] == allSquares[8] && allSquares[2] != 0){
+    }else if(allSquares[2] === allSquares[5] && allSquares[5] === allSquares[8] && allSquares[2] !== 0){
       this.setState({winner: allSquares[2]});
-    }else if(allSquares[2] == allSquares[4] && allSquares[4] == allSquares[6] && allSquares[2] != 0){
+    }else if(allSquares[2] === allSquares[4] && allSquares[4] === allSquares[6] && allSquares[2] !== 0){
       this.setState({winner: allSquares[2]});
-    }else if(allSquares[0] == allSquares[4] && allSquares[4] == allSquares[8] && allSquares[0] != 0){
+    }else if(allSquares[0] === allSquares[4] && allSquares[4] === allSquares[8] && allSquares[0] !== 0){
       this.setState({winner: allSquares[0]});
     }
 
@@ -72,7 +67,6 @@ class GameContainer extends Component {
         <Winner winner={this.state.winner}/>
         <GameGrid squares={this.state.squares} onSquareSelected={this.handleSquareSelected}/>
         <button onClick={this.handleReset}>Reset</button>
-
       </div>
     )
   }
