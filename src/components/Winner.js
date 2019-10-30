8ff1290aed81a9980efd7616cfb482f56096606d
KJeffree <k.jeffree@gmail.com>
2019-10-30 15:19:46 +0000

    player2 text now blinks

--- src/components/Winner.js
+++ src/components/Winner.js
@@ -4,7 +4,7 @@ const Winner = ({winner}) => {
   if (winner == 1){
     return (<h1 className="blinking">Player 1 Wins!</h1>)
   } else if (winner == 2){
-    return (<h1>Player 2 Wins!</h1>)
+    return (<h1 className="blinking">Player 2 Wins!</h1>)
   } else {
     return null;
   }
